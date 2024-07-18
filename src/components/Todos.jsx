5da4bc71565b8e8f6db0e398dbc797f05cfd0048
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-07-18 10:34:52 -0500

    created a simple redux toolkit project

---
+++ src/components/Todos.jsx
@@ -0,0 +1,38 @@
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+
+const Todos = () => {
+  const todos = useSelector((state) => state.todos);
+
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div>Todos</div>
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            key={todo.id}
+          >
+            <div className="text-white">{todo.text}</div>
+            <button className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+            onClick={() => dispatch(removeTodo(todo.id))}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" />
+              </svg>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Todos;
